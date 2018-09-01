b132dc7d97b7269792f551a178381eb491be4c1e
Hendrik Sollich <hendrik@hoodie.de>
2018-09-01 18:46:32 +0200

    much iteration, so todo

--- src/main.js
+++ src/main.js
@@ -4,23 +4,23 @@ import App from './App.html';
 
 const store =  new Store({
 		items: [
-			{ name: "Milch", category: "shopping", done: false },
-			{ name: "Milch", category: "shopping", done: true},
-			{ name: "Milch", category: "shopping", done: true},
-			{ name: "Milch", category: "shopping", done: true},
-			{ name: "Milch", category: "shopping", done: false },
+			//{ name: "Milch", category: "shopping", price: .96, done: false },
 		],
 		catalogue: {
 			items: [
 				{ name: "Milch",    category: "shopping", price: .85 },
 				{ name: "Butter",   category: "shopping", price: 2.29},
-				{ name: "Mehl",     category: "shopping" },
-				{ name: "Bier",     category: "shopping" },
-				{ name: "Saugen",   category: "wohnung"  },
-				{ name: "Hingehen", category: "arbeit"   },
+				{ name: "Mehl",     category: "shopping", price: .6 },
+				{ name: "Bier",     category: "shopping", price: 20 },
+
+				{ name: "TÜV",   category: "AUTO"},
+				{ name: "Waschen",   category: "AUTO"},
+
+				{ name: "Saugen",   category: "wohnung"},
+				{ name: "Küche",    category: "wohnung"},
+				{ name: "Bad",      category: "wohnung"},
 			]
 		},
-		showCatalogue: false,
 	});
 
 store.on('state', ({current}) => {
