24345cf3f6c6f0b76d958ce4c39c7354326d2bbb
Hendrik Sollich <hendrik@hoodie.de>
2018-09-04 20:41:20 +0200

    localStorage and checkbox simplification

--- src/main.js
+++ src/main.js
@@ -2,28 +2,37 @@ import { Store } from "svelte/store";
 
 import App from './App.html';
 
-const store =  new Store({
+const storageKey = 'zutun';
+
+const store = new Store({
+	items: [
+		//{ name: "Milch", category: "shopping", price: .96, done: false },
+	],
+	catalogue: {
 		items: [
-			//{ name: "Milch", category: "shopping", price: .96, done: false },
-		],
-		catalogue: {
-			items: [
-				{ name: "Milch",    category: "shopping", price: .85 },
-				{ name: "Butter",   category: "shopping", price: 2.29},
-				{ name: "Mehl",     category: "shopping", price: .6 },
-				{ name: "Bier",     category: "shopping", price: 20 },
+			{ name: "Milch", category: "shopping", price: .85 },
+			{ name: "Butter", category: "shopping", price: 2.29 },
+			{ name: "Mehl", category: "shopping", price: .6 },
+			{ name: "Bier", category: "shopping", price: 20 },
 
-				{ name: "TÜV",   category: "AUTO"},
-				{ name: "Waschen",   category: "AUTO"},
+			{ name: "TÜV", category: "AUTO" },
+			{ name: "Waschen", category: "AUTO" },
 
-				{ name: "Saugen",   category: "wohnung"},
-				{ name: "Küche",    category: "wohnung"},
-				{ name: "Bad",      category: "wohnung"},
-			]
-		},
-	});
+			{ name: "Saugen", category: "wohnung" },
+			{ name: "Küche", category: "wohnung" },
+			{ name: "Bad", category: "wohnung" },
+		]
+	},
+});
+
+const stored = JSON.parse(localStorage.getItem(storageKey));
+if (stored) {
+	store.set(stored)
+};
 
 store.on('state', ({current}) => {
+	const {catalogue, items} = current;
+	localStorage.setItem(storageKey, JSON.stringify({catalogue, items}));
 	console.dir({storeState: current})
 });
 
